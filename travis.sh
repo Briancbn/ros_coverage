9f6a5b84ddc483f6fd9f8609d66c7ce167a712f1
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-15 17:22:08 +0000

    Adding initial version of travis.sh file

---
+++ travis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export AFTER_SCRIPT='bash <(curl -s https://raw.githubusercontent.com/rarrais/ros_coverage/master/codacy.sh)'
+export BUILDER='catkin_make'
+export ADDITIONAL_DEBS='python-coverage curl jq gcovr'
+export DOCKER_RUN_OPTS='-e CATKIN_TEST_COVERAGE=1 -e TRAVIS_COMMIT -e CODACY_PROJECT_TOKEN'
