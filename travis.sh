843d240ebe175fc3991ae785be86c8302119994f
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-15 18:06:48 +0000

    Changed curl to wget on travis.sh

--- travis.sh
+++ travis.sh
@@ -2,9 +2,9 @@
 
 echo "Exporting env variables"
 
-export AFTER_SCRIPT='bash <(curl -s https://raw.githubusercontent.com/rarrais/ros_coverage/master/codacy.sh)'
+export AFTER_SCRIPT='wget -O - https://raw.githubusercontent.com/rarrais/ros_coverage/master/codacy.sh | bash'
 export BUILDER='catkin_make'
-export ADDITIONAL_DEBS='python-coverage curl jq gcovr'
+export ADDITIONAL_DEBS='python-coverage jq gcovr'
 export DOCKER_RUN_OPTS='-e CATKIN_TEST_COVERAGE=1 -e TRAVIS_COMMIT -e CODACY_PROJECT_TOKEN'
 
 printenv 
