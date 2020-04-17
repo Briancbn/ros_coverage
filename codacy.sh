8b2176f97b040a706c4e5d75a18891cb493dc078
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-17 15:49:59 +0000

    Checking if tests exist before doing any operation

--- codacy.sh
+++ codacy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# cd to the target workspace
-cd /root/target_ws/
-
-# Combine .coverage files & Combine result to xml
-python -m coverage combine `find . -type f -name .coverage`
-python -m coverage xml -o coverage_py.xml
-
-# Generate Cpp coverage xml
-gcovr --xml-pretty > coverage_cpp.xml
+ws="/root/target_ws"
 
 # Setup & Upload Codacy Reporter
 curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-chmod +x codacy-coverage-reporter
+chmod +x $ws/codacy-coverage-reporter
 
-# Report Python coverage
-./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage_py.xml
+# Combine .coverage files & Combine result to xml & Report Python coverage to Codacy
+python -m coverage combine `find $ws -type f -name .coverage` 
+if [ $? -eq 0 ]; then
+  python -m coverage xml -o $ws/coverage_py.xml
+  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r $ws/coverage_py.xml
+fi
 
-# Report C++ coverage
-./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT --language Cpp --force-language -r coverage_cpp.xml
+# Generate C++ coverage xml & Report C++ coverage to Codacy
+if [[ ! -z `find $ws -type f -name *.gcda` ]]; then
+  gcovr -r $ws --xml-pretty > $ws/coverage_cpp.xml
+  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT --language Cpp --force-language -r $ws/coverage_cpp.xml
+fi
