c9e68485f36b9e945dcb35d9bf4d966110f53554
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-17 16:14:18 +0000

    Fix for permissions, changing directory to workspace

--- codacy.sh
+++ codacy.sh
@@ -2,19 +2,22 @@
 
 ws="/root/target_ws"
 
+# Chang directory to ws, as to not trigger permission errors
+cd $ws
+
 # Setup & Upload Codacy Reporter
 curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-chmod +x $ws/codacy-coverage-reporter
+chmod +x codacy-coverage-reporter
 
 # Combine .coverage files & Combine result to xml & Report Python coverage to Codacy
-(cd $ws && exec python -m coverage combine `find $ws -type f -name .coverage`)
+python -m coverage combine `find . -type f -name .coverage`
 if [ $? -eq 0 ]; then
-  (cd $ws && exec python -m coverage xml -o $ws/coverage_py.xml)
-  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r $ws/coverage_py.xml
+  python -m coverage xml -o coverage_py.xml
+  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage_py.xml
 fi
 
-# Generate C++ coverage xml & Report C++ coverage to Codacy
+# # Generate C++ coverage xml & Report C++ coverage to Codacy
 if [[ ! -z `find $ws -type f -name *.gcda` ]]; then
-  gcovr -r $ws --xml-pretty > $ws/coverage_cpp.xml
-  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT --language Cpp --force-language -r $ws/coverage_cpp.xml
+  gcovr -r $ws --xml-pretty > coverage_cpp.xml
+  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT --language Cpp --force-language -r coverage_cpp.xml
 fi
