4b60a7af463ace735e90e184bbaea32940fb6fb3
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-23 15:51:47 +0000

    Attempting to escape double quotes

--- ros_coverage.sh
+++ ros_coverage.sh
@@ -5,7 +5,7 @@ source <(curl -s https://codecov.io/env)
 add 'CATKIN_TEST_COVERAGE=1'
 add 'CODACY_REPORT'
 add 'CODECOV_REPORT'
-add 'ADDITIONAL_DEBS="python-coverage curl jq gcovr"'
+add "ADDITIONAL_DEBS=\"python-coverage curl jq gcovr\""
 # add 'AFTER_SCRIPT="bash <(curl -s https://raw.githubusercontent.com/rarrais/ros_coverage/develop/.ros_coverage/after_script.sh)"'
 
 if [ "$CODACY_PROJECT_TOKEN" != "" ];
