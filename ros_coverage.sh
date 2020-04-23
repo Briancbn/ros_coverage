43a0bb899d94a8943f05f6f3d3bafc053c81d6eb
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-23 16:22:53 +0000

    New attempt at simplifying the process

--- ros_coverage.sh
+++ ros_coverage.sh
@@ -6,8 +6,7 @@ add "CATKIN_TEST_COVERAGE=1"
 add "CODACY_REPORT"
 add "CODECOV_REPORT"
 add "ADDITIONAL_DEBS='python-coverage curl jq gcovr'"
-
-# add 'AFTER_SCRIPT="bash <(curl -s https://raw.githubusercontent.com/rarrais/ros_coverage/develop/.ros_coverage/after_script.sh)"'
+add "AFTER_SCRIPT='bash <(curl -s https://raw.githubusercontent.com/rarrais/ros_coverage/develop/.ros_coverage/after_script.sh)'"
 
 if [ "$CODACY_PROJECT_TOKEN" != "" ];
 then
