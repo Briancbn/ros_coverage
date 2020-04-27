665563a03b85f9974bc27f716da7948d74f8bda3
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-27 17:17:42 +0000

    Solving bug on code_coverage_analysis and introducing changes to remainder functions

--- .ros_coverage/after_script.sh
+++ .ros_coverage/after_script.sh
@@ -6,7 +6,7 @@ function code_coverage_analysis {
     local target_ws=$1
 
     # Python code coverage
-    if python -m coverage combine "$(find "$target_ws" -type f -name .coverage)"; then
+    if python -m coverage combine "$(find $target_ws -type f -name .coverage)"; then
         python -m coverage xml -o "$target_ws"/coverage_py.xml
     fi
 
@@ -21,39 +21,39 @@ function code_coverage_report {
   local target_ws=$1
 
   if [[ "$CODACY" == "true" ]]; then
-    curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-    chmod +x codacy-coverage-reporter
+    curl -Ls -o "$target_ws"/codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
+    chmod +x "$target_ws"/codacy-coverage-reporter
 
     if [[ -f $target_ws/coverage_py.xml ]]; then
-      ./codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" -l Python -r coverage_py.xml
+      ./"$target_ws"/codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" -l Python -r "$target_ws"/coverage_py.xml
     fi
     
     if [[ -f $target_ws/coverage_cpp.xml ]]; then
-      ./codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" --language Cpp --force-language -r coverage_cpp.xml
+      ./"$target_ws"/codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" --language Cpp --force-language -r "$target_ws"/coverage_cpp.xml
     fi
   fi
 
   if [[ "$CODECOV" == "true" ]]; then
-    curl -s https://codecov.io/bash > .codecov
-    chmod +x .codecov
+    curl -s https://codecov.io/bash > "$target_ws"/.codecov
+    chmod +x "$target_ws"/.codecov
 
     if [[ -f $target_ws/coverage_py.xml ]]; then
-      ./.codecov -f coverage_py.xml -cF python
+      ./"$target_ws"/.codecov -f "$target_ws"/coverage_py.xml -cF python
     fi
     
     if [[ -f $target_ws/coverage_cpp.xml ]]; then
-      ./.codecov -f coverage_cpp.xml -cF cpp
+      ./"$target_ws"/.codecov -f "$target_ws"/coverage_cpp.xml -cF cpp
     fi
   fi
 }
 
-code_coverage_threshold_check()
+function code_coverage_threshold_check
 {
   local target_ws=$1
 
   # Python code coverage threshold check
   if [[ -f $target_ws/coverage_py.xml ]]; then
-    python -m coverage report -m --fail-under="$PY_COV_THRESHOLD"
+    (cd "$target_ws" && python -m coverage report -m --fail-under="$PY_COV_THRESHOLD")
     if [[ $? -eq 2 && "$PY_FAIL_UNDER_COV_THRESHOLD" == "true" ]]; then
       echo "Failed! Under Python Threshold criteria."
       exit 2
@@ -77,9 +77,6 @@ PY_COV_THRESHOLD=80
 
 CPP_FAIL_UNDER_COV_THRESHOLD="false"
 CPP_COV_THRESHOLD=80
-
-# Chang directory to target_ws, as to not trigger permission errors
-cd $target_ws || exit
 
 code_coverage_analysis target_ws
 code_coverage_report target_ws
