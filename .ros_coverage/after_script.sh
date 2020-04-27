6282a0423706e2116c45eaa92b319978c8bd055c
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-27 17:37:08 +0000

    Using auxiliary function to exec command in ROS workspace

--- .ros_coverage/after_script.sh
+++ .ros_coverage/after_script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
+function exec_in_workspace {
+    local target_ws=$1; shift
+    ( cd "$target_ws" && exec "$@")
+}
+
 function code_coverage_analysis {
     echo "Running Code Coverage Analysis!"
 
     local target_ws=$1
 
     # Python code coverage
-    if python -m coverage combine "$(find $target_ws -type f -name .coverage)"; then
-        python -m coverage xml -o "$target_ws"/coverage_py.xml
+    if exec_in_workspace "$target_ws" python -m coverage combine "$(find . -type f -name .coverage)"; then
+        exec_in_workspace "$target_ws" python -m coverage xml -o "$target_ws"/coverage_py.xml
     fi
 
     # C++ code coverage
-    if [[ ! -z $(find "$target_ws" -type f -name '*.gcda') ]]; then
+    if [[ ! -z $(find $target_ws -type f -name '*.gcda') ]]; then
         gcovr -r "$target_ws" --xml-pretty > "$target_ws"/coverage_cpp.xml
     fi
 }
@@ -21,28 +26,28 @@ function code_coverage_report {
   local target_ws=$1
 
   if [[ "$CODACY" == "true" ]]; then
-    curl -Ls -o "$target_ws"/codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-    chmod +x "$target_ws"/codacy-coverage-reporter
+    exec_in_workspace "$target_ws" curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
+    exec_in_workspace "$target_ws" chmod +x codacy-coverage-reporter
 
     if [[ -f $target_ws/coverage_py.xml ]]; then
-      ./"$target_ws"/codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" -l Python -r "$target_ws"/coverage_py.xml
+      exec_in_workspace "$target_ws" ./codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" -l Python -r "$target_ws"/coverage_py.xml
     fi
     
     if [[ -f $target_ws/coverage_cpp.xml ]]; then
-      ./"$target_ws"/codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" --language Cpp --force-language -r "$target_ws"/coverage_cpp.xml
+      exec_in_workspace "$target_ws" ./codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" --language Cpp --force-language -r "$target_ws"/coverage_cpp.xml
     fi
   fi
 
   if [[ "$CODECOV" == "true" ]]; then
-    curl -s https://codecov.io/bash > "$target_ws"/.codecov
-    chmod +x "$target_ws"/.codecov
+    exec_in_workspace "$target_ws" curl -s https://codecov.io/bash > .codecov
+    exec_in_workspace "$target_ws" chmod +x .codecov
 
     if [[ -f $target_ws/coverage_py.xml ]]; then
-      ./"$target_ws"/.codecov -f "$target_ws"/coverage_py.xml -cF python
+      exec_in_workspace "$target_ws" ./.codecov -f "$target_ws"/coverage_py.xml -cF python
     fi
     
     if [[ -f $target_ws/coverage_cpp.xml ]]; then
-      ./"$target_ws"/.codecov -f "$target_ws"/coverage_cpp.xml -cF cpp
+      exec_in_workspace "$target_ws" ./.codecov -f "$target_ws"/coverage_cpp.xml -cF cpp
     fi
   fi
 }
@@ -53,7 +58,7 @@ function code_coverage_threshold_check
 
   # Python code coverage threshold check
   if [[ -f $target_ws/coverage_py.xml ]]; then
-    (cd "$target_ws" && python -m coverage report -m --fail-under="$PY_COV_THRESHOLD")
+    exec_in_workspace "$target_ws" python -m coverage report -m --fail-under="$PY_COV_THRESHOLD"
     if [[ $? -eq 2 && "$PY_FAIL_UNDER_COV_THRESHOLD" == "true" ]]; then
       echo "Failed! Under Python Threshold criteria."
       exit 2
