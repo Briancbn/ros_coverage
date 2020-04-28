ac05364c9243d6316c5457130d3aaf2ec0d13f37
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-28 10:51:03 +0000

    Changing the way target_ws is passed and used in functions

--- .ros_coverage/after_script.sh
+++ .ros_coverage/after_script.sh
@@ -1,64 +1,54 @@
 #!/bin/bash
 
-function exec_in_workspace {
-    local target_ws=$1; shift
-    ( cd "$target_ws" && exec "$@")
-}
-
 function code_coverage_analysis {
-    echo "Running Code Coverage Analysis!"
+    local target_ws="$1";
 
-    local target_ws=$1
-
-    # Python code coverage
-    if exec_in_workspace "$target_ws" python -m coverage combine "$(find . -type f -name .coverage)"; then
-        exec_in_workspace "$target_ws" python -m coverage xml -o "$target_ws"/coverage_py.xml
+    # shellcheck disable=SC2046
+    if (cd "$target_ws" && python -m coverage combine $(find . -type f -name .coverage);)  then
+      (cd "$target_ws" && python -m coverage xml -o coverage_py.xml)
     fi
 
-    # C++ code coverage
-    if [[ ! -z $(find $target_ws -type f -name '*.gcda') ]]; then
-        gcovr -r "$target_ws" --xml-pretty > "$target_ws"/coverage_cpp.xml
+    if [[ ! -z $(find "$target_ws" -type f -name '*.gcda') ]]; then
+      gcovr -r "$target_ws" --xml-pretty > "$target_ws"/coverage_cpp.xml
     fi
 }
 
 function code_coverage_report {
-
   local target_ws=$1
 
   if [[ "$CODACY" == "true" ]]; then
-    exec_in_workspace "$target_ws" curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-    exec_in_workspace "$target_ws" chmod +x codacy-coverage-reporter
+    (cd "$target_ws" && curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')") 
+    chmod +x "$target_ws"/codacy-coverage-reporter
 
     if [[ -f $target_ws/coverage_py.xml ]]; then
-      exec_in_workspace "$target_ws" ./codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" -l Python -r "$target_ws"/coverage_py.xml
+      "$target_ws"/codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" -l Python -r "$target_ws"/coverage_py.xml
     fi
     
     if [[ -f $target_ws/coverage_cpp.xml ]]; then
-      exec_in_workspace "$target_ws" ./codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" --language Cpp --force-language -r "$target_ws"/coverage_cpp.xml
+      "$target_ws"/codacy-coverage-reporter report --commit-uuid "$TRAVIS_COMMIT" --language Cpp --force-language -r "$target_ws"/coverage_cpp.xml
     fi
   fi
 
   if [[ "$CODECOV" == "true" ]]; then
-    exec_in_workspace "$target_ws" curl -s https://codecov.io/bash > .codecov
-    exec_in_workspace "$target_ws" chmod +x .codecov
+    (cd "$target_ws" && curl -s https://codecov.io/bash > .codecov)
+    chmod +x "$target_ws"/.codecov
 
     if [[ -f $target_ws/coverage_py.xml ]]; then
-      exec_in_workspace "$target_ws" ./.codecov -f "$target_ws"/coverage_py.xml -cF python
+      "$target_ws"/.codecov -f "$target_ws"/coverage_py.xml -cF python
     fi
     
     if [[ -f $target_ws/coverage_cpp.xml ]]; then
-      exec_in_workspace "$target_ws" ./.codecov -f "$target_ws"/coverage_cpp.xml -cF cpp
+      "$target_ws"/.codecov -f "$target_ws"/coverage_cpp.xml -cF cpp
     fi
   fi
 }
 
-function code_coverage_threshold_check
-{
+function code_coverage_threshold_check {
   local target_ws=$1
 
   # Python code coverage threshold check
   if [[ -f $target_ws/coverage_py.xml ]]; then
-    exec_in_workspace "$target_ws" python -m coverage report -m --fail-under="$PY_COV_THRESHOLD"
+    (cd "$target_ws" && python -m coverage report -m --fail-under="$PY_COV_THRESHOLD")
     if [[ $? -eq 2 && "$PY_FAIL_UNDER_COV_THRESHOLD" == "true" ]]; then
       echo "Failed! Under Python Threshold criteria."
       exit 2
@@ -83,6 +73,6 @@ PY_COV_THRESHOLD=80
 CPP_FAIL_UNDER_COV_THRESHOLD="false"
 CPP_COV_THRESHOLD=80
 
-code_coverage_analysis target_ws
-code_coverage_report target_ws
-code_coverage_threshold_check target_ws
+code_coverage_analysis "$target_ws"
+code_coverage_report $target_ws
+code_coverage_threshold_check $target_ws
